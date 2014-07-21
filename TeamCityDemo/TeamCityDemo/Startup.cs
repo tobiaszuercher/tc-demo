8694fa8749476fc32ee35f3bb812f558a1f9a842
Tobias Zürcher <tobias.zuercher@gmail.com>
2014-07-21 16:03:17 +0200

    init commit

---
+++ TeamCityDemo/TeamCityDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamCityDemo.Startup))]
+namespace TeamCityDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
